14bed7b9e77b97ac44c4a29b1ed5f12e1f1d21f9
Anders Riutta <git@andersriutta.com>
2018-01-29 12:11:41 -0800

    Better error handling/reporting

--- GPMLConverter.php
+++ GPMLConverter.php
@@ -1,45 +1,57 @@
 <?php
 namespace WikiPathways;
 
-# TODO do we want to use trigger_error and try/catch/finally, or is it enough to just return false?
 function write_to_stream($pipes, $proc) {
+	wfDebug("write_to_stream(" . json_encode($pipes) . ", " . json_encode($proc) . ") called\n");
 	return function($data, $end) use($pipes, $proc) {
-		$stdin = $pipes[0];
-		$stdout = $pipes[1];
-		$stderr = $pipes[2];
+		try{
+			$stdin = $pipes[0];
+			$stdout = $pipes[1];
+			$stderr = $pipes[2];
 
-		fwrite($stdin, $data);
+			fwrite($stdin, $data);
 
-		if (!isset($end) || $end != true) {
-			return write_to_stream($pipes, $proc);
+			if (!isset($end) || $end != true) {
+				wfDebug("Ending stream\n");
+				return write_to_stream($pipes, $proc);
+			}
+
+			fclose($stdin);
+
+			$result = stream_get_contents($stdout);
+			$info = stream_get_meta_data($stdout);
+			$err = stream_get_contents($stderr);
+
+			fclose($stderr);
+
+			if ($info['timed_out']) {
+				wfDebug("Error: pipe timed out\n");
+				trigger_error('pipe timed out', E_USER_NOTICE);
+			}
+
+
+			proc_close($proc);
+
+			if ($err) {
+				wfDebug("Error in write_to_stream():\n");
+				wfDebug($err);
+				wfDebug("\n");
+				trigger_error($err, E_USER_NOTICE);
+			}
+
+			return $result;
+		} catch(Exception $e) {
+			proc_close($proc);
+			wfDebug("Error in write_to_stream():\n");
+			wfDebug($e);
+			wfDebug("\n");
+			trigger_error($e, E_USER_NOTICE);
 		}
-
-		fclose($stdin);
-
-		$result = stream_get_contents($stdout);
-		$info = stream_get_meta_data($stdout);
-		$err = stream_get_contents($stderr);
-
-		fclose($stderr);
-
-		if ($info['timed_out']) {
-			#trigger_error('pipe timed out', E_USER_NOTICE);
-			return false;
-		}
-
-
-		proc_close($proc);
-
-		if ($err) {
-			#trigger_error($err, E_USER_NOTICE);
-			return false;
-		}
-
-		return $result;
 	};
 };
 
 function create_stream($cmd, $opts = array()) {
+	wfDebug("create_stream($cmd, " . json_encode($opts) . ") called\n");
 	$timeout = $opts["timeout"];
 
 	$proc = proc_open("cat - | $cmd",
@@ -62,11 +74,12 @@ function create_stream($cmd, $opts = array()) {
 
 		return write_to_stream($pipes, $proc);
 	} else {
-		#trigger_error("Error: $proc for $cmd must be a resource.", E_USER_NOTICE);
+		wfDebug("Error: $proc for $cmd must be a resource.");
 		return false;
 	}
 }
 
+# TODO do we want to use trigger_error and try/catch/finally, or is it enough to just return false?
 class GPMLConverter{
 	// TODO is there a better way to define these?
 	public static $gpml2pvjson_path="/nix/var/nix/profiles/default/bin/gpml2pvjson";
@@ -85,8 +98,7 @@ class GPMLConverter{
 		$pvjs_path = self::$pvjs_path;
 
 		if (empty($gpml)) {
-			echo "Error: invalid gpml provided:<br>";
-			echo $gpml;
+			wfDebug("Error: invalid gpml provided");
 			return;
 		}
 
@@ -98,77 +110,74 @@ class GPMLConverter{
 $gpml2pvjson_path --id $identifier --pathway-version $version | \
 $jq_path -rc '. as {\$pathway} | (.entityMap | .[] |= (.type += if .dbId then [.dbConventionalName + ":" + .dbId] else [] end )) as \$entityMap | {\$pathway, \$entityMap}'
 TEXT;
-		//TODO: this timeout should be removed when we get async cacheing working
-		$streamGpml2Pvjson = create_stream("$toPvjsonCmd", array("timeout" => 4));
-		$rawPvjsonString = $streamGpml2Pvjson($gpml, true);
-
-		/* TODO we disabled all unification, because it was overloading the server. We need to do caching or something.
-		return $rawPvjsonString;
-		//*/
-
-		/*
-		//Skip bridgedb unification unless view=widget (i.e., where the unification is useful)
-		$view = isset($_GET["view"]) ? $_GET["view"] : "normal";
-		if($view != 'widget')
+		$rawPvjsonString = '';
+		try{
+			//TODO: this timeout should be updated or removed when we get async caching working
+			$streamGpml2Pvjson = create_stream("$toPvjsonCmd", array("timeout" => 10));
+			$rawPvjsonString = $streamGpml2Pvjson($gpml, true);
+		} catch(Exception $e) {
+			wfDebug("Error converting GPML to PVJSON:");
+			wfDebug($e);
+			wfDebug("\n");
 			return $rawPvjsonString;
-		//*/
-
-## TODO the enrich method from bridgedbjs is extremely slow when this was
-## installed via Nix, but it may have been faster when installed via NPM.
-## Regardless, it's currently much slower than xrefsBatch, suggesting the
-## batching is not happening below.
-#		$enrichCmd = <<<TEXT
-#$jq_path -rc '.entityMap[]' | \
-#$bridgedb_path enrich $organism dbConventionalName dbId ncbigene ensembl wikidata | \
-#$jq_path -rc --slurp 'reduce .[] as \$entity ({}; .[\$entity.id] = \$entity)';
-#TEXT;
+		}
 
 		$xrefsBatchCmd = <<<TEXT
-$jq_path -rc '.entityMap[] | select(has("dbId") and has("dbConventionalName") and .gpmlElementName == "DataNode" and .dbConventionalName != "undefined" and .dbId != "undefined") | .dbConventionalName + "," + .dbId' | \
+$jq_path -rc '.entityMap[] | select(has("dbId") and has("dbConventionalName") and .gpmlElementName == "DataNode" and (.wpType == "GeneProduct" or .wpType == "Protein" or .wpType == "Rna" or .wpType == "Metabolite") and .dbConventionalName != "undefined" and .dbId != "undefined") | .dbConventionalName + "," + .dbId' | \
 $bridgedb_path xrefsBatch --organism $organism | \
 $jq_path -rc --slurp 'reduce .[] as \$entity ({}; .[\$entity.dbConventionalName + ":" + \$entity.dbId] = \$entity)';
 TEXT;
-		//TODO: this timeout should be removed when we get async cacheing working
-		$writeToBridgeDbStream = create_stream("$xrefsBatchCmd", array("timeout" => 4));
-		$bridgedbResultString = $writeToBridgeDbStream($rawPvjsonString, true);
+		$bridgedbResultString = '';
+
+		try{
+			//TODO: this timeout should be updated or removed when we get async caching working
+			$writeToBridgeDbStream = create_stream("$xrefsBatchCmd", array("timeout" => 10));
+			$bridgedbResultString = $writeToBridgeDbStream($rawPvjsonString, true);
+		} catch(Exception $e) {
+			wfDebug("Error using BridgeDb to unify Xrefs:");
+			wfDebug($e);
+			wfDebug("\n");
+			return $rawPvjsonString;
+		}
+
+
 		// TODO Are we actually saving any time by doing this instead of just parsing it as JSON?
 		if (!$bridgedbResultString || empty($bridgedbResultString) || $bridgedbResultString == '{}' || $bridgedbResultString == '[]') {
 			return $rawPvjsonString;
 		}
 
-# TODO should we use this? NOTE: if yes, be sure to update the timeout!
-#		try{
-#			$writeToBridgeDbStream = create_stream("$xrefsBatchCmd", array("timeout" => 30));
-#			$bridgedbResultString = $writeToBridgeDbStream($rawPvjsonString, true);
-#		} catch(Exception $e) {
-#			return $rawPvjsonString;
-#		}
-
-		$bridgedbResult = json_decode($bridgedbResultString);
-		$pvjson = json_decode($rawPvjsonString);
-		$pathway = $pvjson->pathway;
-		$entityMap = $pvjson->entityMap;
-		foreach ($entityMap as $key => $value) {
-			if (property_exists($value, 'dbConventionalName') && property_exists($value, 'dbId')) {
-				$xrefId = $value->dbConventionalName.":".$value->dbId;
-				if (property_exists($bridgedbResult, $xrefId)) {
-					$mapper = $bridgedbResult->$xrefId;
-					if (property_exists($mapper, 'xrefs')) {
-						$xrefs = $mapper->xrefs;
-						foreach ($xrefs as $xref) {
-							if (property_exists($xref, 'isDataItemIn') && property_exists($xref, 'dbId')) {
-								$datasource = $xref->isDataItemIn;
-								if (property_exists($datasource, 'preferredPrefix')) {
-									array_push($value->type, "$datasource->preferredPrefix:$xref->dbId");
+		try{
+			$bridgedbResult = json_decode($bridgedbResultString);
+			$pvjson = json_decode($rawPvjsonString);
+			$pathway = $pvjson->pathway;
+			$entityMap = $pvjson->entityMap;
+			foreach ($entityMap as $key => $value) {
+				if (property_exists($value, 'dbConventionalName') && property_exists($value, 'dbId')) {
+					$xrefId = $value->dbConventionalName.":".$value->dbId;
+					if (property_exists($bridgedbResult, $xrefId)) {
+						$mapper = $bridgedbResult->$xrefId;
+						if (property_exists($mapper, 'xrefs')) {
+							$xrefs = $mapper->xrefs;
+							foreach ($xrefs as $xref) {
+								if (property_exists($xref, 'isDataItemIn') && property_exists($xref, 'dbId')) {
+									$datasource = $xref->isDataItemIn;
+									if (property_exists($datasource, 'preferredPrefix')) {
+										array_push($value->type, "$datasource->preferredPrefix:$xref->dbId");
+									}
 								}
 							}
 						}
 					}
 				}
 			}
-		}
 
-		return json_encode(array("pathway"=>$pathway, "entityMap"=>$entityMap));
+			return json_encode(array("pathway"=>$pathway, "entityMap"=>$entityMap));
+		} catch(Exception $e) {
+			wfDebug("Error integrating unified xrefs with pvjson:");
+			wfDebug($e);
+			wfDebug("\n");
+			return $rawPvjsonString;
+		}
 	}
 
 	public static function pvjson2svg($pvjson, $opts) {
@@ -176,15 +185,24 @@ TEXT;
 		$pvjs_path = self::$pvjs_path;
 
 		if (empty($pvjson) || trim($pvjson) == '{}') {
-			echo "Error: invalid pvjson provided:<br>";
-			echo $pvjson;
+			wfDebug("Error: invalid pvjson provided\n");
+			wfDebug(json_encode($pvjson));
+			wfDebug("\n");
 			return;
 		}
 
 		$static = isset($opts["static"]) ? $opts["static"] : false;
 
-		$streamPvjsonToSvg = create_stream("$pvjs_path json2svg -s $static", array("timeout" => 2));
-		return $streamPvjsonToSvg($pvjson, true);
+		try{
+			$streamPvjsonToSvg = create_stream("$pvjs_path json2svg -s $static", array("timeout" => 10));
+			return $streamPvjsonToSvg($pvjson, true);
+		} catch(Exception $e) {
+			wfDebug("Error converting PVJSON to SVG:");
+			wfDebug($e);
+			wfDebug("\n");
+			return;
+		}
+
 	}
 
 }
