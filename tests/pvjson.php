612af9363ca5dc0463ef9563e74d4c65f82c98fe
Anders Riutta <git@andersriutta.com>
2018-02-12 13:29:09 -0800

    Re-enable theming. Parse inputs as alphanumberic.

--- tests/pvjson.php
+++ tests/pvjson.php
@@ -4,10 +4,17 @@ error_reporting(E_ALL);
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 //*/
-require_once "GPMLConverter.php";
+require_once "../src/GPMLConverter.php";
+use WikiPathways\GPMLConverter;
 header("Access-Control-Allow-Origin: *");
-$identifier = isset($_GET["identifier"]) ? $_GET["identifier"] : "WP4";
-$version = isset($_GET["version"]) ? $_GET["version"] : "0";
+
+function alphanumeric($s) {
+	return preg_replace("/[^a-zA-Z0-9]+/", "", $s);
+}
+
+
+$identifier = isset($_GET["identifier"]) ? alphanumeric($_GET["identifier"]) : "WP4";
+$version = isset($_GET["version"]) ? alphanumeric($_GET["version"]) : "0";
 
 $gpml = base64_decode(json_decode(file_get_contents("https://webservice.wikipathways.org/getPathwayAs?fileType=gpml&pwId=$identifier&format=json"))->data);
 $gpml_parsed = new SimpleXMLElement($gpml);
--- tests/svg.php
+++ tests/svg.php
@@ -2,10 +2,16 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
-require_once "GPMLConverter.php";
+require_once "../src/GPMLConverter.php";
+use WikiPathways\GPMLConverter;
 
-$identifier = isset($_GET["identifier"]) ? $_GET["identifier"] : "WP4";
-$version = isset($_GET["version"]) ? $_GET["version"] : "0";
+function alphanumeric($s) {
+	return preg_replace("/[^a-zA-Z0-9]+/", "", $s);
+}
+
+$identifier = isset($_GET["identifier"]) ? alphanumeric($_GET["identifier"]) : "WP4";
+$version = isset($_GET["version"]) ? alphanumeric($_GET["version"]) : "0";
+$theme = isset($_GET["theme"]) ? alphanumeric($_GET["theme"]) : "plain";
 
 $gpml = base64_decode(json_decode(file_get_contents("https://webservice.wikipathways.org/getPathwayAs?fileType=gpml&pwId=$identifier&format=json"))->data);
 $gpml_parsed = new SimpleXMLElement($gpml);
@@ -15,4 +21,4 @@ $pvjson = GPMLConverter::gpml2pvjson($gpml, array("identifier"=>$identifier, "ve
 
 #echo 'why';
 #echo $pvjson;
-echo GPMLConverter::pvjson2svg($pvjson, array("static"=>false));
+echo GPMLConverter::pvjson2svg($pvjson, array("react"=>false, "theme"=>$theme));
