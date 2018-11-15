65cc9c29af7019f5dfdcb5a8d369e436afeb3cc1
Anders Riutta <git@andersriutta.com>
2018-11-15 03:23:44 -0800

    Add more error handling

--- bulk.sh
+++ bulk.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
-xmlstarlet='/nix/store/dwigzvk3yrbai9mxh3k2maqsghfjqgr6-xmlstarlet-1.6.1/bin/xmlstarlet'
-for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml' | head -n 100); do
-  $xmlstarlet val "$f";
+# see https://stackoverflow.com/a/246128/5354298
+get_script_dir() { echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; }
+SCRIPT_DIR=$(get_script_dir)
 
-  if [ $? -eq 0 ]; then
+LOG_FILE="/home/wikipathways.org/bulk-conversion.log"
+INVALID_GPML_LIST="/home/wikipathways.org/invalid-gpml-list.txt"
+rm "$INVALID_GPML_LIST"
+touch "$INVALID_GPML_LIST"
+
+# drawing on https://codeinthehole.com/tips/bash-error-reporting/
+function handle_error {
+  read line file <<<$(caller)
+  echo "An error occurred in line $line of file $file:" | tee -a "$LOG_FILE" >&2
+  sed "${line}q;d" "$file" | tee -a "$LOG_FILE" >&2
+  exit 1
+}
+
+trap handle_error ERR
+
+    echo "$f" >> "/home/wikipathways.org/invalid-gpml.txt"
+
+xmlstarlet='/nix/store/dwigzvk3yrbai9mxh3k2maqsghfjqgr6-xmlstarlet-1.6.1/bin/xmlstarlet'
+for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml'); do
+  # TODO: which is better?
+  #$xmlstarlet val "$f";
+  #if [ $? -eq 0 ]; then ... fi
+  is_valid=$(($xmlstarlet val "$f" | grep ' valid') || echo '');
+  echo "$f"
+  if [[ -n "$is_valid" ]]; then
     sudo -i "$(pwd)/gpmlconverter" "$f";
    
     dir_f=$(dirname "$f")
@@ -15,5 +39,9 @@ for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml' | head -n 100); 
    
     sudo chown www-data:www-data "$prefix".*
     sudo chmod 644 "$prefix".*
+  else
+    echo "  Warning: invalid GPML"
+    echo " "
+    echo "$f" >> "$INVALID_GPML_LIST"
   fi
 done
