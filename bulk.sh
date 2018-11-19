3cc5b843f665d95f0b18edb5e42f60eaca19bb01
Anders Riutta <git@andersriutta.com>
2018-11-18 22:52:25 -0800

    Get bulk.sh error handling working better

--- bulk.sh
+++ bulk.sh
@@ -4,28 +4,52 @@
 get_script_dir() { echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; }
 SCRIPT_DIR=$(get_script_dir)
 
-LOG_FILE="/home/wikipathways.org/bulk-conversion.log"
-INVALID_GPML_LIST="/home/wikipathways.org/invalid-gpml-list.txt"
-rm "$INVALID_GPML_LIST"
+TIMESTAMP=$(date +"%Y-%m-%d-%H%M%S")
+
+LOG_FILE="$HOME/bulk$TIMESTAMP.log"
+INVALID_GPML_LIST="$HOME/invalid-gpml$TIMESTAMP.txt"
+touch "$LOG_FILE"
 touch "$INVALID_GPML_LIST"
+
+# Based on http://linuxcommand.org/lc3_wss0140.php
+PROGNAME=$(basename $0)
+error_exit()
+{
+
+#	----------------------------------------------------------------
+#	Function for exit due to fatal program error
+#		Accepts 1 argument:
+#			string containing descriptive error message
+#	----------------------------------------------------------------
+
+
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
 
 # drawing on https://codeinthehole.com/tips/bash-error-reporting/
 function handle_error {
   read line file <<<$(caller)
-  echo "An error occurred in line $line of file $file:" | tee -a "$LOG_FILE" >&2
-  sed "${line}q;d" "$file" | tee -a "$LOG_FILE" >&2
+  (echo "An error occurred in line $line of file $file:" | tee -a "$LOG_FILE" >&2)
+  (sed "${line}q;d" "$file" | tee -a "$LOG_FILE" >&2)
   exit 1
 }
 
 trap handle_error ERR
 
+TARGET_FORMAT="$1"
+TARGET_FORMAT="${TARGET_FORMAT:-*}"
+
 xmlstarlet='/nix/store/dwigzvk3yrbai9mxh3k2maqsghfjqgr6-xmlstarlet-1.6.1/bin/xmlstarlet'
-for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml'); do
+#for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml'); do
+for f in $(find /home/wikipathways.org/images/wikipathways/7/78 -name 'WP*.gpml'); do
   # TODO: which is better?
   #$xmlstarlet val "$f";
   #if [ $? -eq 0 ]; then ... fi
   is_valid=$(($xmlstarlet val "$f" | grep ' valid') || echo '');
-  echo "$f"
+  echo '' | tee -a "$LOG_FILE"
+  echo '------------------------------------------------' | tee -a "$LOG_FILE"
+  echo "$f" | tee -a "$LOG_FILE"
   if [[ -n "$is_valid" ]]; then
     dir_f=$(dirname "$f")
     base_f=$(basename -- "$f")
@@ -33,15 +57,18 @@ for f in $(find /home/wikipathways.org/images/ -name 'WP*.gpml'); do
     stub_f="${base_f%.*}"
     prefix="$dir_f/$stub_f"
 
-    # If you want to convert all:
-    #sudo -i "$SCRIPT_DIR/bin/gpml2" "$f";
-    # Convert the minimum required to get svgs:
-    sudo -i "$SCRIPT_DIR/bin/gpml2" "$f" "$prefix.svg";
+    if [[ $TARGET_FORMAT == '*' ]]; then
+      # If you want to convert all:
+      sudo -i "$SCRIPT_DIR/bin/gpml2" "$f" 2> >(tee -a "$LOG_FILE" >&2);
+    else
+      # Convert the minimum required to get svgs:
+      sudo -i "$SCRIPT_DIR/bin/gpml2" "$f" "$prefix.$TARGET_FORMAT" 2> >(tee -a "$LOG_FILE" >&2);
+    fi
    
     sudo chown www-data:www-data "$prefix".*
     sudo chmod 644 "$prefix".*
   else
-    echo "  Warning: invalid GPML"
+    echo "  Warning: invalid GPML" | tee -a "$LOG_FILE"
     echo " "
     echo "$f" >> "$INVALID_GPML_LIST"
   fi
