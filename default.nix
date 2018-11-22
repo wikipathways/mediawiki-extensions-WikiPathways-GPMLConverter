53d84bff2fe526ae7487b92d939a3feadaa749ab
Anders Riutta <git@andersriutta.com>
2018-11-21 17:41:24 -0800

    default.nix needs to be in here

---
+++ default.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { config.allowUnfree = true; };
+let
+  custom = import ./nixpkgs-custom/all-custom.nix;
+  pathvisio = callPackage ./nixpkgs-custom/pathvisio/default.nix {
+    organism="Homo sapiens";
+    headless=true;
+    genes=false;
+    interactions=false;
+    metabolites=false;
+    # NOTE: this seems high, but I got an error
+    #       regarding memory when it was lower.
+    memory="2048m";
+  };
+in [
+  pathvisio
+  custom.bridgedb
+  custom.gpml2pvjson
+  custom.pvjs
+
+  pkgs.php
+  php72Packages.composer
+
+  pkgs.coreutils
+  pkgs.xmlstarlet
+] ++ (if stdenv.isDarwin then [] else [])
